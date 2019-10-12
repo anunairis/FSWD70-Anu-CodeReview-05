107a5313b456d2a97a1654c477ff7876725c7892
Anu Nairis <anu.nairis90@gmail.com>
2019-10-12 14:06:55 +0300

    finishes codereview

--- script_Js_Json/script.js
+++ script_Js_Json/script.js
@@ -2,7 +2,6 @@ const jsonMovie = movie;
 for(let i = 0; i < jsonMovie.length; i++ ) {
   const htmlElement = `
     <div class="relative_position col-lg-5 col-md-5 col-sm-12 col-xs-12 mb-5 change_bg_dark hover_row"> 
-
       <div class="row">
           <img id="img_${jsonMovie[i].id}" class="col-lg-5 d-flex align-self-stretch py-3 rounded" src="${jsonMovie[i].image}"></img>
           <div class="col-lg-7">
@@ -10,19 +9,14 @@ for(let i = 0; i < jsonMovie.length; i++ ) {
             <p class="text-white">Published year: ${jsonMovie[i].year}</p>
             <p class="text-white">${jsonMovie[i].plot}</p>
 
-
-
           <div class="likes_container d-flex flex-nowrap justify-content-end align-items-end pb-2"> 
-        
             <button type="button" id="btn_${jsonMovie[i].id}" class="btn btn-default btn-lg bg-warning mr-2"> 
             <span class="fa">&#xf0a4;</span> Like
             </button>
               <span class="text-white rounded rounded-circle bg-warning px-3 py-2"><span id="counter_span_${jsonMovie[i].id}">0</span>
           </div>
-
-
-          </div>
         </div>
+      </div>
     </div>
   `;
   const oneMovie = $(htmlElement);
@@ -34,5 +28,4 @@ for(let i = 0; i < jsonMovie.length; i++ ) {
     $(`#counter_span_${jsonMovie[i].id}`).text(counter);
   });
 }
-
 
